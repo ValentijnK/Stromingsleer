71ec94cba08dad50a97f0078e558c0f18d7fabbb
Valentijn Kilian <valentijnkilian@gmail.com>
2020-12-01 17:15:21 +0100

    Toegevoegd: pompdruk en vermogen berekenen

--- main.py
+++ main.py
@@ -109,6 +109,30 @@ def drukval_hoogte():
     return round(ph, 3)
 
 
+def pompdruk():
+    global rho
+    rho = rho
+    v1 = float(input("Wat is de inlaat snelheid in m/s (eerste segment)?"))
+    v2 = float(input("Wat is de uitlaat snelheid in m/s (laatste segment)?"))
+    pw = float(input("Wat is de totale drukval door wrijving en hoogte in bar?"))
+    p1 = float(input("Wat is de zuigdruk van de pomp in bar?"))
+    p2 = float(input("Wat is de minimale gewenste druk in bar?"))
+
+    pd = (p2*10**5 + 0.5 * rho * v2**2 + pw*10**5) - (p1*10**5 + 0.5 * rho * v1**2)
+    pd = round(pd/10**5, 3)
+
+    print("De opvoerdruk van de pomp is: ", pd, "bar")
+    return pd
+
+
+def pomp_vermogen():
+    pd = float(input("Wat is de opvoerdruk van de pomp in bar?"))
+    vol = float(input("Wat is het volumedebiet in m3/h?"))
+
+    pv = pd * (vol/36)
+    pv = round(pv, 2)
+    print("Het benodigde pompvermogen is: ", pv, "kW")
+    return pv
 # Kies hier welke functies je wilt aanroepen
 
 # reynolds_number(5.44) # Getal van reynolds berekenen
@@ -116,7 +140,9 @@ def drukval_hoogte():
 # velocity() #  snelheid berekenen
 # pressure_loss()  # Drukval in leiding berekenen
 # pressure_system()  # Drukval door compleet leidingsegment berekenen.
-drukval_hoogte()  # Drukval door hoogte berekenen.
+# drukval_hoogte()  # Drukval door hoogte berekenen.
+# pompdruk()  # Opvoerdruk van een pomp berekenen.
+pomp_vermogen()  # Pompvermogen berekenen in kW.
 # friction_coefficient()  # Wrijvings coefficient berekenen.
 # Press the green button in the gutter to run the script.
 # Copyright (C) 2020  Valentijn Kilian
